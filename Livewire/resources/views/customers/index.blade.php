38e283d4ac377a6a4d369c4e6812995afeb04c7d
AlisonCardoso <alisoncardoso7@gmail.com>
2024-09-23 17:14:57 -0300

    import tailwind css

---
+++ Livewire/resources/views/customers/index.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Customers') }}
+        </h2>
+    </x-slot>
+
+    <div class="w-10/12 mt-10 m-auto min-h-screen bg-white">
+       
+    </div>
+</x-app-layout>
